cdd95e705ac118be3dc834a366dc21c882c2358f
yMqtheus <79208440+yMqtheus@users.noreply.github.com>
2021-02-20 00:53:53 -0300

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/556993733829 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO BOT:*
+_https://chat.whatsapp.com/J2ZGIuyEx1hAUmjjIqXZD5 `
+}
+exports.daftarvip = daftarvip
