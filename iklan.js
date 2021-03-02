43452dcedc7dfbe825525129bb3399452808a880
yMqtheus <79208440+yMqtheus@users.noreply.github.com>
2021-03-02 09:58:45 -0300

    Add files via upload

---
+++ iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 25 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *PIC PAY, MERCADO PAGO, GIFT CARD*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/553493354233*
+║
+╚═〘 *_FihDo~Theus~_* 〙
+`
+}
+exports.iklan = iklan
